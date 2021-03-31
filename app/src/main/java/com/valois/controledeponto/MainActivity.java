bdd902f61463c2c0635a7d8c2568a24815ee9275
leonanvalois <eng.leonan@gmail.com>
2021-03-31 13:59:57 -0300

    Criacao da activity home

--- app/src/main/java/com/valois/controledeponto/MainActivity.java
+++ app/src/main/java/com/valois/controledeponto/MainActivity.java
@@ -2,7 +2,11 @@ package com.valois.controledeponto;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Inicializar componentes
+        Button btn_entrar = findViewById(R.id.btn_entrar);
+
+
+        btn_entrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Olá usuário", Toast.LENGTH_SHORT);
+                toast.show();
+
+                Intent irParaHome = new Intent(MainActivity.this,ActivityHome.class);
+                startActivity(irParaHome);
+            }
+        });
+
     }
+
+
 }
