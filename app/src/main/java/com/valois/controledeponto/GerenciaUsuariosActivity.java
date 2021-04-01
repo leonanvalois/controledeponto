925e8adba9607646edc44660ad4f51c404080c31
leonanvalois <eng.leonan@gmail.com>
2021-04-01 20:19:44 -0300

    correcoes de nomenclatura e design

--- app/src/main/java/com/valois/controledeponto/GerenciaUsuariosActivity.java
+++ app/src/main/java/com/valois/controledeponto/GerenciaUsuariosActivity.java
@@ -3,17 +3,14 @@ package com.valois.controledeponto;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.database.DataSnapshot;
@@ -27,7 +24,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-public class MainActivity extends AppCompatActivity {
+public class GerenciaUsuariosActivity extends AppCompatActivity {
 
     EditText edt_nome, edt_email, edt_senha;
     ListView list_usuarios;
@@ -45,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_gerencia_usuarios);
 
         edt_nome = findViewById(R.id.edt_nome);
         edt_email = findViewById(R.id.edt_email);
@@ -78,7 +75,7 @@ public class MainActivity extends AppCompatActivity {
                     listUsuario.add(u);
                 }
 
-                arrayAdapterUsuario = new ArrayAdapter<Usuario>(MainActivity.this, android.R.layout.simple_list_item_1, listUsuario);
+                arrayAdapterUsuario = new ArrayAdapter<Usuario>(GerenciaUsuariosActivity.this, android.R.layout.simple_list_item_1, listUsuario);
                 list_usuarios.setAdapter(arrayAdapterUsuario);
             }
 
@@ -90,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void inicializarFirebase() {
-        FirebaseApp.initializeApp(MainActivity.this);
+        FirebaseApp.initializeApp(GerenciaUsuariosActivity.this);
         firebaseDatabase = FirebaseDatabase.getInstance();
         firebaseDatabase.setPersistenceEnabled(true);
         databaseReference = firebaseDatabase.getReference();
