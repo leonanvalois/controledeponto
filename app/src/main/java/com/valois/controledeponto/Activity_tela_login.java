7e0e472d8f5802410a31e3c59c272074963edd0a
leonanvalois <eng.leonan@gmail.com>
2021-04-01 18:20:32 -0300

    Construcao da tela de login

---
+++ app/src/main/java/com/valois/controledeponto/Activity_tela_login.java
@@ -0,0 +1,14 @@
+package com.valois.controledeponto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_tela_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_login);
+    }
+}
