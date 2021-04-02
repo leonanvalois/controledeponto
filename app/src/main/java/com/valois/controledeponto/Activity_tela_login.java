df58dbd14dcff058b7dda63b79b56192e6180b3f
valois <lleandrovalois@gmail.com>
2021-04-02 01:35:04 -0300

    Finalizado o controle CRUD e integracao com firebase

--- app/src/main/java/com/valois/controledeponto/Activity_tela_login.java
+++ app/src/main/java/com/valois/controledeponto/Activity_tela_login.java
@@ -1,27 +1,83 @@
 package com.valois.controledeponto;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.valois.controledeponto.Utils.Conexao;
 
 public class Activity_tela_login extends AppCompatActivity {
 
+    FirebaseAuth auth;
+    EditText edt_email, edt_senha;
     Button btn_entrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_login);
-        btn_entrar = findViewById(R.id.btn_login_entrar);
 
+        //inicializa todos os componentes da view
+        inicilizarComponentes();
+        //responsável pelos eventos de click dos botões
+        eventos();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth = Conexao.getFirebaseAuth();
+    }
+
+    private void inicilizarComponentes() {
+        btn_entrar = findViewById(R.id.btn_login_entrar);
+        edt_email = findViewById(R.id.edt_login_email);
+        edt_senha = findViewById(R.id.edt_login_senha);
+        edt_email.setText("lleandrovalois@gmail.com");
+        edt_senha.setText("161002");
+    }
+
+    private void eventos() {
         btn_entrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent entrar = new Intent(Activity_tela_login.this, GerenciaUsuariosActivity.class);
-                startActivity(entrar);
+                //Intent i = new Intent(Activity_tela_login.this, GerenciaUsuariosActivity.class);
+                //startActivity(i);
+                autenticaUsuario();
             }
         });
+    }
+
+    private void autenticaUsuario() {
+        if (edt_email.getText().toString().isEmpty()) {
+            alert("Informe o seu e-mail!");
+        } else if (edt_senha.getText().toString().isEmpty()) {
+            alert("Informe sua senha!");
+        } else {
+            String email = edt_email.getText().toString().trim();
+            String senha = edt_senha.getText().toString().trim();
+            auth.signInWithEmailAndPassword(email, senha).addOnCompleteListener(Activity_tela_login.this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Intent i = new Intent(Activity_tela_login.this, ActivityHome.class);
+                        startActivity(i);
+                    } else {
+                        alert(task.getResult().toString());
+                    }
+                }
+            });
+        }
+    }
+
+    private void alert(String s) {
+        Toast.makeText(Activity_tela_login.this,s,Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/valois/controledeponto/GerenciaUsuariosActivity.java
+++ app/src/main/java/com/valois/controledeponto/GerenciaUsuariosActivity.java
@@ -3,7 +3,11 @@ package com.valois.controledeponto;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,13 +15,19 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.valois.controledeponto.Utils.Conexao;
 import com.valois.controledeponto.modelo.Usuario;
 
 import java.util.ArrayList;
@@ -28,6 +38,7 @@ public class GerenciaUsuariosActivity extends AppCompatActivity {
 
     EditText edt_nome, edt_email, edt_senha;
     ListView list_usuarios;
+    private FirebaseAuth firebaseAuth;
 
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
@@ -35,7 +46,7 @@ public class GerenciaUsuariosActivity extends AppCompatActivity {
     private List<Usuario> listUsuario = new ArrayList<Usuario>();
     private ArrayAdapter<Usuario> arrayAdapterUsuario;
 
-    Usuario usuarioSelecionado ;
+    Usuario usuarioSelecionado;
 
     String no_principal = "Usuario";
 
@@ -48,21 +59,43 @@ public class GerenciaUsuariosActivity extends AppCompatActivity {
         edt_email = findViewById(R.id.edt_email);
         edt_senha = findViewById(R.id.edt_senha);
         list_usuarios = findViewById(R.id.list_usuarios);
-
+        Toast.makeText(GerenciaUsuariosActivity.this, "ola", Toast.LENGTH_LONG);
         inicializarFirebase();
 
         eventoDatabase();
 
-       list_usuarios.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-           @Override
-           public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-               usuarioSelecionado = (Usuario) parent.getItemAtPosition(position);
-               edt_nome.setText(usuarioSelecionado.getNome());
-               edt_email.setText(usuarioSelecionado.getEmail());
-               edt_senha.setText(usuarioSelecionado.getSenha());
-           }
-       });
+        list_usuarios.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                usuarioSelecionado = (Usuario) parent.getItemAtPosition(position);
+                edt_nome.setText(usuarioSelecionado.getNome());
+                edt_email.setText(usuarioSelecionado.getEmail());
+                edt_senha.setText(usuarioSelecionado.getSenha());
+            }
+        });
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth = Conexao.getFirebaseAuth();
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        new AlertDialog.Builder(GerenciaUsuariosActivity.this)
+                .setTitle("Você será desconectado")
+                .setMessage("Tem certeza que deseja sair do sistema?")
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        System.exit(0);
+                    }
+                })
+                .setNegativeButton(android.R.string.no, null)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .show();
     }
 
     private void eventoDatabase() {
@@ -70,7 +103,7 @@ public class GerenciaUsuariosActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 listUsuario.clear();
-                for (DataSnapshot objSnapshot: snapshot.getChildren()){
+                for (DataSnapshot objSnapshot : snapshot.getChildren()) {
                     Usuario u = objSnapshot.getValue(Usuario.class);
                     listUsuario.add(u);
                 }
@@ -91,6 +124,7 @@ public class GerenciaUsuariosActivity extends AppCompatActivity {
         firebaseDatabase = FirebaseDatabase.getInstance();
         firebaseDatabase.setPersistenceEnabled(true);
         databaseReference = firebaseDatabase.getReference();
+
     }
 
     @Override
@@ -104,40 +138,75 @@ public class GerenciaUsuariosActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         switch (id) {
-            case R.id.menu_novo:
-                Usuario usuario = new Usuario();
-                usuario.setUid(UUID.randomUUID().toString());
-                usuario.setNome(edt_nome.getText().toString());
-                usuario.setEmail(edt_email.getText().toString());
-                usuario.setSenha(edt_senha.getText().toString());
-                databaseReference.child(no_principal).child(usuario.getUid()).setValue(usuario);
-                limparCampos();
-                break;
-
-            case R.id.menu_salvar:
-                Usuario uAtualiza = new Usuario();
-                uAtualiza.setUid(usuarioSelecionado.getUid());
-                uAtualiza.setNome(edt_nome.getText().toString().trim());
-                uAtualiza.setEmail(edt_email.getText().toString().trim());
-                uAtualiza.setSenha(edt_senha.getText().toString().trim());
-                databaseReference.child(no_principal).child(uAtualiza.getUid()).setValue(uAtualiza);
-                limparCampos();
+            case R.id.menu_atualizar:
+                if (edt_email.getText().toString().isEmpty() ) {
+                    alert("Preencha os campos ou seleciona um usuário para editar.");
+                } else {
+                    Usuario uAtualiza = new Usuario();
+                    if (usuarioSelecionado == null){
+                        uAtualiza.setUid(UUID.randomUUID().toString());
+                    } else  {
+                        uAtualiza.setUid(usuarioSelecionado.getUid());
+                    }
+                    uAtualiza.setNome(edt_nome.getText().toString().trim());
+                    uAtualiza.setEmail(edt_email.getText().toString().trim());
+                    uAtualiza.setSenha(edt_senha.getText().toString().trim());
+                    databaseReference.child(no_principal).child(uAtualiza.getUid()).setValue(uAtualiza);
+                    limparCampos();
+                }
                 break;
 
             case R.id.menu_apagar:
-                Usuario uDelete = new Usuario();
-                uDelete.setUid(usuarioSelecionado.getUid());
-                databaseReference.child(no_principal).child(uDelete.getUid()).removeValue();
-                limparCampos();
+                if (edt_email.getText().toString().isEmpty() ) {
+                    alert("Não há nada pra apagar");
+                } else {
+                    new AlertDialog.Builder(GerenciaUsuariosActivity.this)
+                            .setTitle("O usuário " + edt_nome.getText() + " será excluído.")
+                            .setMessage("Deseja realmente excluir o usuário?")
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    Usuario uDelete = new Usuario();
+                                    uDelete.setUid(usuarioSelecionado.getUid());
+                                    databaseReference.child(no_principal).child(uDelete.getUid()).removeValue();
+                                    alert("Usuário excluído com sucesso.");
+                                    limparCampos();
+                                }
+                            })
+                            .setNegativeButton(android.R.string.no, null)
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+
+                }
                 break;
         }
 
         return true;
     }
 
+    private void criarUsuario(String email, String senha) {
+        firebaseAuth.createUserWithEmailAndPassword(email, senha).addOnCompleteListener(GerenciaUsuariosActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    alert("Usuário cadastrado com sucesso");
+                    Intent i = new Intent(getApplicationContext(), Activity_tela_login.class);
+                    startActivity(i);
+                    finish();
+                } else {
+                    alert(task.getResult().toString());
+                }
+            }
+        });
+    }
+
+    private void alert(String s) {
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+    }
+
     private void limparCampos() {
         edt_nome.setText("");
         edt_email.setText("");
         edt_senha.setText("");
+        usuarioSelecionado = null;
     }
 }
